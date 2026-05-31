3be7af2e6dbd8fb971add389f1158b10c825463a
vortexishereforhelp-creator <vortexishereforhelp@gmail.com>
2026-05-31 18:40:16 +0100

    Update Main.m

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"https://api.novafn.dev"
+#define API_URL @"http://141.95.54.204:7575:3551"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
